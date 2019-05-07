e1f6c30e659af25e503ce407dc2393f94da7bcef
ViktorOnegin <33310165+ViktorOnegin@users.noreply.github.com>
2019-05-07 12:57:38 +0300

    xd

---
+++ FirstApp/FirstApp/FirstApp/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+
+namespace FirstApp
+{
+    public partial class MainPage : ContentPage
+    {
+        string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
+        public MainPage()
+        {
+            InitializeComponent();
+
+            if (File.Exists(_fileName))
+            {
+                editor.Text = File.ReadAllText(_fileName);
+            }
+        }
+
+        void OnSaveButtonClicked(Object sender, EventArgs args)
+        {
+            editor.Text = File.ReadAllText(_fileName);
+        }
+
+        void OnDeleteButtonClicked(Object sender, EventArgs args)
+        {
+            if (File.Exists(_fileName))
+            {
+                File.Delete(_fileName);
+            }
+            editor.Text = string.Empty;
+        }
+    }
+}
---
+++ TipCalculatorWithForms/TipCalc/TipCalc/TipCalc/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TipCalc
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        //Label tip;
+        //Label bill;
+        //Label tipAmount;
+        //Label totalAmount;
+        //Button calculate;
+        //Slider slider;
+
+        public double SliderValue { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+           
+
+            slider.ValueChanged += Slider_ValueChanged;
+
+
+        }
+
+        public void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            SliderValue = ((Slider)sender).Value;
+        }
+
+    }
+}
